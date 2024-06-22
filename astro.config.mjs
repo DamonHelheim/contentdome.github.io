b40f9f58e07cdb7dd0ad69a47c03c1aefff1d2d2
damonhelheim <damonhelheim@gmail.com>
2024-06-22 11:59:51 +0200

    add actions 7

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from 'astro/config'
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+
+const LIVE_URL = 'https://damonhelheim.github.io';
+const SUB_URL = 'contentdome.github.io';
 
 export default defineConfig({
-  site: 'https://damonhelheim.github.io',
-  base: 'contentdome.github.io',
+  site: LIVE_URL,
+  base: SUB_URL,
 })
